bb6046147b0e81802ad189aba25ec6973d4d573a
Constantinescu Paul <paulconst7000@gmail.com>
2021-05-14 22:59:04 +0300

    forgot something

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -51,25 +51,30 @@ public class Main {
 //        }
 
 //        Etapa 2
-//        final MedicService medicService = MedicService.getInstance();
-//        final MedicService2 medicService2 = MedicService2.getInstance();
-//        System.out.println(medicService.getMedics());
+        final MedicService medicService = MedicService.getInstance();
+        final MedicService2 medicService2 = MedicService2.getInstance();
+        medicService2.saveMedics();
+        System.out.println(medicService.getMedics());
 
-//        final ClientService clientService = ClientService.getInstance();
-//        final ClientService2 clientService2 = ClientService2.getInstance();
-//        System.out.println(clientService.getClients());
+        final ClientService clientService = ClientService.getInstance();
+        final ClientService2 clientService2 = ClientService2.getInstance();
+        clientService2.saveClients();
+        System.out.println(clientService.getClients());
 
-//        final CabinetService cabinetService = CabinetService.getInstance();
-//        final CabinetService2 cabinetService2 = CabinetService2.getInstance();
-//        System.out.println(cabinetService.getCabinets());
+        final CabinetService cabinetService = CabinetService.getInstance();
+        final CabinetService2 cabinetService2 = CabinetService2.getInstance();
+        cabinetService2.saveCabinets();
+        System.out.println(cabinetService.getCabinets());
 
-//        final ProgramareService programareService = ProgramareService.getInstance();
-//        final ProgramareService2 programareService2 = ProgramareService2.getInstance();
-//        System.out.println(programareService.getProgramares());
+        final ProgramareService programareService = ProgramareService.getInstance();
+        final ProgramareService2 programareService2 = ProgramareService2.getInstance();
+        programareService2.saveProgramares();
+        System.out.println(programareService.getProgramares());
 
-//        final RetetaService retetaService = RetetaService.getInstance();
-//        final RetetaService2 retetaService2 = RetetaService2.getInstance();
-//        System.out.println(retetaService.getRetetas());
+        final RetetaService retetaService = RetetaService.getInstance();
+        final RetetaService2 retetaService2 = RetetaService2.getInstance();
+        retetaService2.saveRetetas();
+        System.out.println(retetaService.getRetetas());
 
     }
 }
